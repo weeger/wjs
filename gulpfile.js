f314b276f35d4842bdc733b6c2e0610eee3840c7
Romain WEEGER <romain.weeger@gmail.com>
2016-05-28 10:20:56 +0200

    Update info files

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+/**
+ * To install dependencies run :
+ * npm i -D gulp gulp-babel babel-preset-es2015 gulp-sourcemaps gulp-uglify gulp-concat
+ */
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+
+gulp.task('buildCoreJs', () => {
+  return gulp.src('src/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(concat('w.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('buildExtensionsJs', () => {
+  return gulp.src('extension/*.js','!extension/*.min.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(['src/*.js'], ['buildCoreJs']);
+  gulp.watch(['extension/*.js','!extension/*.min.js'], ['buildExtensionsJs']);
+});
+
+gulp.task('default', ['watch']);
