ef67b215bfbb073244c5343bad0eedbbec92ab5e
Romain WEEGER <romain.weeger@gmail.com>
2016-05-29 14:31:36 +0200

    Scroll manager

--- extension/JsClassStatic/ScrollManager.js
+++ extension/JsClassStatic/ScrollManager.js
@@ -2,18 +2,25 @@
   'use strict';
   /**
    * Simulate scroll for interactive management.
+   * @require JsClassStatic > MousePositionListener
    */
   W.register('JsClassStatic', 'ScrollManager', {
     scrollX: null,
     scrollY: null,
     scrollSpeedX: false,
     scrollSpeedY: false,
+    mouseDriftListeners: [],
 
     __construct: function () {
       this.windowScrollBind = this.windowScroll.bind(this);
       this.windowResizeBind = this.windowResize.bind(this);
       this.w.window.addEventListener('scroll', this.windowScrollBind);
       this.w.window.addEventListener('resize', this.windowResizeBind);
+      this.mouseWheelX = function (e) {
+        this.w.window.scrollTo(this.w.ScrollManager.scrollX + e.deltaY, this.w.ScrollManager.scrollY);
+      }.bind(this);
+      this.mouseMoveDriftOverBind = this.mouseMoveDriftOver.bind(this);
+      this.mouseMoveDriftIntervalBind = this.mouseMoveDriftInterval.bind(this);
     },
 
     __destruct: function () {
@@ -63,6 +70,60 @@
     scrollGet: function (direction) {
       var method = 'scroll' + direction;
       return this.w.document.body[method] || this.w.document.documentElement[method];
+    },
+
+    mouseWheelEnable: function (direction) {
+      if (direction === 'X') {
+        this.w.window.addEventListener('mousewheel', this.mouseWheelX);
+      }
+    },
+
+    mouseWheelDisable: function (direction) {
+      if (direction === 'X') {
+        this.w.window.removeEventListener('mousewheel', this.mouseWheelX);
+      }
+    },
+
+    mouseDriftEnable: function (direction, innerLimit, callback) {
+      // Add a listener the first time.
+      if (!this.mouseDriftListeners.length) {
+        this.w.window.addEventListener('mouseover', this.mouseMoveDriftOverBind);
+      }
+      this.mouseDriftListeners.push({direction: direction, limit: innerLimit, callback: callback});
+    },
+
+    mouseMoveDriftOver: function (e) {
+      e.target.addEventListener('mouseout', this.mouseMoveDriftOut.bind(this));
+      this.mouseMoveDriftInterval();
+    },
+
+    mouseMoveDriftOut: function () {
+      this.w.window.clearInterval(this.mouseMoveDriftTe);
+    },
+
+    mouseMoveDriftInterval: function () {
+      for (var i = 0, value, item; item = this.mouseDriftListeners[i++];) {
+        value = this.w.MousePositionListener['client' + item.direction];
+        // Min position.
+        if (value < item.limit ||
+          // Max position.
+          value > (this.w.window['inner' + (item.direction === 'X' ? 'Width' : 'Height')] - item.limit)) {
+          // Launch callback.
+          item.callback(value);
+        }
+      }
+      this.mouseMoveDriftTe = this.w.window.setTimeout(this.mouseMoveDriftIntervalBind, 10);
+    },
+
+    mouseDriftDisable: function (callback) {
+      for (var i = 0, item; item = this.mouseDriftListeners[i++];) {
+        if (item.callback === callback) {
+          this.w.arrayDeleteItem(this.mouseDriftListeners, item);
+        }
+      }
+      if (!this.mouseDriftListeners.length) {
+        this.w.window.removeEventListener('mouseover', this.mouseMoveDriftOverBind);
+      }
     },
 
     windowResize: function () {
